91294017905d8e8416bc053a6e2a72b52f82fe07
marsh5 <brodiemarshall567@gmail.com>
2022-06-14 16:34:53 -0400

    added addToCart feature

--- src/contexts/user.contex.jsx
+++ src/contexts/user.contex.jsx
@@ -3,6 +3,7 @@ import { createContext, useState, useEffect } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener, signOutUser } from "../utils/firebase/firebase.utils";
 
 //the actual value you want to access
+//the defaultValue argument is helpful for testing components in isolation without wrapping them, or testing it with different values from the Provider.
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
@@ -25,5 +26,7 @@ export const UserProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
+  //Every Context object comes with a Provider React component that allows consuming components to subscribe to context changes.
+  //Provider component accepts a value prop to be passed to consuming components that are descendants of this Provider
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
